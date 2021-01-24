90ede9c3dce9e2302c97ad5f6754496dc1cec8b5
Goroh <Goroh@Goroh-PC>
2021-01-25 01:53:53 +0200

    first task

---
+++ vko2/src/vko2/Dog.java
@@ -0,0 +1,11 @@
+package vko2;
+
+public class Dog {
+    public Dog(String name){
+        System.out.println("Hei, nimeni on " + name + "!");
+    }
+    
+    public void speak(String text){
+        System.out.println(text);
+    }
+}
