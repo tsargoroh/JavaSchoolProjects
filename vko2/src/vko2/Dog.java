fba559f8075c2c2c11aa2ced93ab28a57b4b8b6f
Goroh <Goroh@Goroh-PC>
2021-01-25 03:19:42 +0200

    first task

--- vko2/src/vko2/Dog.java
+++ vko2/src/vko2/Dog.java
@@ -1,11 +1,38 @@
 package vko2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Dog {
+    private String name;
+    private String says;
+    
     public Dog(String name){
-        System.out.println("Hei, nimeni on " + name + "!");
+        if (name.trim().isEmpty() == true)
+            name = "Doge";
+        System.out.println("Hey, my name is " + name + "!");
+        this.name = name;
+        this.says = "Much wow!";
     }
     
     public void speak(String text){
-        System.out.println(text);
+        while (text.trim().isEmpty() == true){
+            System.out.println(says);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("What does a dog say: ");
+            try {
+                text = br.readLine();
+            } catch (IOException ex) {
+            }
+        }
+        Scanner scanner = new Scanner(text);
+        while (scanner.hasNext()){
+            if (scanner.hasNextBoolean())
+                System.out.println("Such boolean: " + scanner.next());
+            else if (scanner.hasNextInt())
+                System.out.println("Such integer: " + scanner.next());
+        }
     }
 }
