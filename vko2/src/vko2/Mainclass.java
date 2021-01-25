867845521840114935af5d63cbe1178d8f58400b
Goroh <Goroh@Goroh-PC>
2021-01-25 02:01:25 +0200

    first task

--- vko2/src/vko2/Mainclass.java
+++ vko2/src/vko2/Mainclass.java
@@ -3,7 +3,10 @@ package vko2;
 
 public class Mainclass {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dog dog1 = new Dog("Rekku");
+        Dog dog2 = new Dog("Musti");
+        dog1.speak("Hau!");
+        dog2.speak("Vuh!");
     }
     
 }
