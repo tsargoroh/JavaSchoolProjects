ddae6e1569fbff34a42e8cb3108f9b25f64799c9
Goroh <Goroh@Goroh-PC>
2021-02-01 22:55:44 +0200

    3rd week 4th task

--- vko3/src/vko3/BottleDispenser.java
+++ vko3/src/vko3/BottleDispenser.java
@@ -1,16 +1,19 @@
 package vko3;
 
+import java.util.ArrayList;
+
 public class BottleDispenser {
     private int bottles;
-    private final Bottle[] bottle_array;
+    private ArrayList<Bottle> bottle_list;
     private int money;
+    private int i;
     
     public BottleDispenser() {
-        bottles = 50;
+        bottles = 5;
         money = 0;
-        bottle_array = new Bottle[bottles];
-        for (int i = 0;i<bottles;i++) {
-            bottle_array[i] = new Bottle();
+        bottle_list = new ArrayList<Bottle>();
+        for (i = 0;i<bottles;i++) {
+            bottle_list.add(new Bottle());
         }
     }
     public void addMoney() {
@@ -18,10 +21,11 @@ public class BottleDispenser {
         System.out.println("Klink! Added more money!");
     }
     public void buyBottle() {
-        if (bottles != 0 && (money - bottle_array[0].getPrice() > 0)) {
+        if (bottles != 0 && (money - bottle_list.get(0).getPrice() > 0)) {
             --bottles;
-            money -= bottle_array[0].getPrice();
-            System.out.println("KACHUNK! " + bottle_array[0].getName() + " came out of the dispenser!");
+            money -= bottle_list.get(0).getPrice();
+            System.out.println("KACHUNK! " + bottle_list.get(0).getName() + " came out of the dispenser!");
+            bottle_list.remove(0);
         } else {
             System.out.println("Add money first!");
         }
@@ -29,5 +33,11 @@ public class BottleDispenser {
     public void returnMoney() {
         money = 0;
         System.out.println("Klink klink. Money came out!");
+    }
+    public void listBottles() {
+        for (i = 0;i<bottles;i++) {
+            System.out.println(i+1 + ". Name: " + bottle_list.get(i).getName());
+            System.out.println("\tSize: " + bottle_list.get(i).getSize() + "\tPrice: " + bottle_list.get(i).getPrice());
+        }
     }
 }
