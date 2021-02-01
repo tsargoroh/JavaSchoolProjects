fce5c15eec5addb0c7574db29c489bf2989e35c9
Goroh <Goroh@Goroh-PC>
2021-02-01 20:27:42 +0200

    3rd week 1st task

--- vko3/src/vko3/Mainclass.java
+++ vko3/src/vko3/Mainclass.java
@@ -2,6 +2,13 @@ package vko3;
 
 public class Mainclass {
     public static void main(String[] args){
-        
+        BottleDispenser bd = new BottleDispenser();
+        bd.addMoney();
+        bd.buyBottle();
+        bd.buyBottle();
+        bd.addMoney();
+        bd.addMoney();
+        bd.buyBottle();
+        bd.returnMoney();
     }
 }
