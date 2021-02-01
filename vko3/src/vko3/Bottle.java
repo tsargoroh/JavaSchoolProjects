3310ee45ea2b27a599bffa59cbe281e737192eaf
Goroh <Goroh@Goroh-PC>
2021-02-01 20:45:29 +0200

    3rd week 2nd task

--- vko3/src/vko3/Bottle.java
+++ vko3/src/vko3/Bottle.java
@@ -1,5 +1,20 @@
 package vko3;
 
 public class Bottle {
-    
+    private String name;
+    private String manufacturer;
+    private float total_energy;
+    public Bottle() {
+        name = "Pepsi Max";
+        manufacturer = "Pepsi";
+        total_energy = 0.3f;
+    }
+    public Bottle(String name, String manuf, double totE){
+        this.name = name;
+        manufacturer = manuf;
+        total_energy = (float) totE;
+    }
+    //public String getName(){}
+    //public String getManufacturer(){}
+    //public double getEnergy(){}
 }
