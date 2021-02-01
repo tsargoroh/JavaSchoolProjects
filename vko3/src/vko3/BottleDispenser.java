6b980688ce3b4886e4f562e1e2ea5024c99525b7
Goroh <Goroh@Goroh-PC>
2021-02-01 20:20:04 +0200

    3rd week 1st task

--- vko3/src/vko3/BottleDispenser.java
+++ vko3/src/vko3/BottleDispenser.java
@@ -1,5 +1,31 @@
 package vko3;
 
 public class BottleDispenser {
+    private int bottles;
+    private int money;
     
+    public BottleDispenser() {
+        bottles = 5;
+        money = 0;
+    }
+    
+    public void addMoney() {
+        money += 1;
+        System.out.println("Klink! Added more money!");
+    }
+    
+    public void buyBottle() {
+        if (bottles != 0 && money != 0) {
+            --bottles;
+            --money;
+            System.out.println("KACHUNK! A bottle came out of the dispenser!");
+        }else if (money == 0) {
+            System.out.println("Add money first!");
+        }
+    }
+    
+    public void returnMoney() {
+        money = 0;
+        System.out.println("Klink klink!! All money gone!");
+    }
 }
