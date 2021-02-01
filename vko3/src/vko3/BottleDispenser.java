a8b253a173a121082a4947ac7726f0b0c676cc8b
Goroh <Goroh@Goroh-PC>
2021-02-01 23:55:06 +0200

    3rd week 5th task

--- vko3/src/vko3/BottleDispenser.java
+++ vko3/src/vko3/BottleDispenser.java
@@ -1,38 +1,49 @@
 package vko3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class BottleDispenser {
     private int bottles;
     private ArrayList<Bottle> bottle_list;
-    private int money;
+    private float money;
     private int i;
-    
+    private String choice;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public BottleDispenser() {
         bottles = 5;
-        money = 0;
+        money = 0f;
         bottle_list = new ArrayList<Bottle>();
-        for (i = 0;i<bottles;i++) {
-            bottle_list.add(new Bottle());
-        }
+        bottle_list.add(new Bottle());
+        bottle_list.add(new Bottle("Pepsi Max", "Pepsi", 0.5f, 1.5f, 2.2f));
+        bottle_list.add(new Bottle("Coca-Cola Zero", "Coca-Cola", 0.5f, 0.5f, 2.0f));
+        bottle_list.add(new Bottle("Coca-Cola Zero", "Coca-Cola", 0.5f, 1.5f, 2.5f));
+        bottle_list.add(new Bottle("Fanta Zero", "Fanta", 0.5f, 0.5f, 1.95f));
     }
     public void addMoney() {
         money += 1;
         System.out.println("Klink! Added more money!");
     }
     public void buyBottle() {
-        if (bottles != 0 && (money - bottle_list.get(0).getPrice() > 0)) {
+        listBottles();
+        System.out.print("Your choice: ");
+        try {
+            choice = br.readLine();
+        } catch (IOException ex) {}
+        if (bottles != 0 && (money - bottle_list.get(Integer.parseInt(choice) - 1).getPrice() > 0)) {
             --bottles;
-            money -= bottle_list.get(0).getPrice();
-            System.out.println("KACHUNK! " + bottle_list.get(0).getName() + " came out of the dispenser!");
-            bottle_list.remove(0);
+            money -= bottle_list.get(Integer.parseInt(choice) - 1).getPrice();
+            System.out.println("KACHUNK! " + bottle_list.get(Integer.parseInt(choice) - 1).getName() + " came out of the dispenser!");
+            bottle_list.remove(Integer.parseInt(choice) - 1);
         } else {
             System.out.println("Add money first!");
         }
     }
     public void returnMoney() {
+        System.out.printf("Klink klink. Money came out! You got %.2f€ back\n", money);
         money = 0;
-        System.out.println("Klink klink. Money came out!");
     }
     public void listBottles() {
         for (i = 0;i<bottles;i++) {
