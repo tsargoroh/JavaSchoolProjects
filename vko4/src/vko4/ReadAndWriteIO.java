1cca39f694cc886f65d5e21e31b2c53d52586188
Goroh <Goroh@Goroh-PC>
2021-02-08 18:09:33 +0200

    4rd week 4th task

--- vko4/src/vko4/ReadAndWriteIO.java
+++ vko4/src/vko4/ReadAndWriteIO.java
@@ -6,11 +6,13 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ReadAndWriteIO {
     private BufferedReader br;
     private BufferedWriter bw;
     private String line;
+    private Scanner sc;
     public void readFile(String fileName) {
         try {
             br = new BufferedReader(new FileReader(fileName));
@@ -29,7 +31,16 @@ public class ReadAndWriteIO {
             br = new BufferedReader(new FileReader(inputFileName));
             bw = new BufferedWriter(new FileWriter(outputFileName));
             while ((line = br.readLine()) != null){
-                bw.write(line + "\n");
+                if (line.length() < 30 && !line.trim().isEmpty()) {
+                    sc = new Scanner(line);
+                    sc.useDelimiter("");
+                    while (sc.hasNext()){
+                        if (sc.next().equals("v")) {
+                            bw.write(line + "\n");
+                            break;
+                        }
+                    }
+                }
             }
             br.close();
             bw.close();
