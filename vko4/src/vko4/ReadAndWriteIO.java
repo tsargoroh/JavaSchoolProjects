047246072cda1da08269276b42ef1224d397dad8
Goroh <Goroh@Goroh-PC>
2021-02-08 18:47:47 +0200

    4rd week 5th task

--- vko4/src/vko4/ReadAndWriteIO.java
+++ vko4/src/vko4/ReadAndWriteIO.java
@@ -1,53 +1,27 @@
 package vko4;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class ReadAndWriteIO {
     private BufferedReader br;
-    private BufferedWriter bw;
     private String line;
-    private Scanner sc;
-    public void readFile(String fileName) {
+    
+    public void readZip(String zipName) {
         try {
-            br = new BufferedReader(new FileReader(fileName));
-            while ((line = br.readLine()) != null){
-                System.out.println(line);
+            ZipFile zip = new ZipFile(zipName);
+            Enumeration<? extends ZipEntry> entries = zip.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                br = new BufferedReader(new InputStreamReader(zip.getInputStream(entry)));
+                while ((line = br.readLine()) != null)
+                        System.out.println(line);
+                br.close();
             }
-            br.close();
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex);
-        } catch (IOException ex) {
-            System.out.println(ex);
-        }
-    }
-    public void readAndWrite(String inputFileName, String outputFileName) {
-        try {
-            br = new BufferedReader(new FileReader(inputFileName));
-            bw = new BufferedWriter(new FileWriter(outputFileName));
-            while ((line = br.readLine()) != null){
-                if (line.length() < 30 && !line.trim().isEmpty()) {
-                    sc = new Scanner(line);
-                    sc.useDelimiter("");
-                    while (sc.hasNext()){
-                        if (sc.next().equals("v")) {
-                            bw.write(line + "\n");
-                            break;
-                        }
-                    }
-                }
-            }
-            br.close();
-            bw.close();
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex);
-        } catch (IOException ex) {
-            System.out.println(ex);
-        }
+        } catch (IOException ex) {}
     }
 }
